8097fcd676d890e0346e26d384c6e852d620d943
engrjeff <engineerjeff15@gmail.com>
2025-10-03 21:08:31 +0800

    feat: add new ui

--- components/test-table.tsx
+++ components/test-table.tsx
@@ -14,21 +14,23 @@ export function TestTable() {
   const { messages } = useMQTTClient();
 
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Topic</TableHead>
-          <TableHead>Message</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {messages.map((message, index) => (
-          <TableRow key={index.toString()}>
-            <TableCell>{message.topic}</TableCell>
-            <TableCell>{message.message}</TableCell>
+    <div className="w-full">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Topic</TableHead>
+            <TableHead>Message</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {messages.map((message, index) => (
+            <TableRow key={index.toString()}>
+              <TableCell>{message.topic}</TableCell>
+              <TableCell>{message.message}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
